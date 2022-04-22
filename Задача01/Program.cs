590b6367619ab30ab3be11c9170697d11da88b4b
Yevgeniya-Lat <e9618281262@gmail.com>
2022-04-22 20:02:15 +0300

    Задача1 Степень двойки

---
+++ Задача01/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задача 1: 
+Определите, является ли число степенью двойки:
+
+N = 16 -> "Является степень двойки"
+N = 12 -> “Не является степенью двойки”
+*/
+
+int Prompt (string massege)
+{
+    Console.WriteLine(massege);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+
+    bool PowerTwq(int a)
+    {
+        if (a == 2) 
+        {
+        return true;
+        }
+
+        else if (a % 2 == 0) 
+        {
+            return PowerTwq(a / 2);
+        }
+        else return false;
+        
+    }
+
+int N = Prompt("Введите число N: ");
+System.Console.WriteLine();
+if (PowerTwq(N)) Console.WriteLine($"{N} -> Является степенью двойки"); 
+else Console.WriteLine($"{N} -> Не является степенью двойки"); 
+
