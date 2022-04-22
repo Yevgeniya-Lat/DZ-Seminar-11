f15db83987a00d670a3aa3cc533995fa29c95d3f
Yevgeniya-Lat <e9618281262@gmail.com>
2022-04-22 23:34:54 +0300

    Положительные числа

---
+++ Задача02/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задача 2: 
+Проверка на простое число:
+
+N = 13 -> "Это простое число"
+N = 12 -> “Это не простое число”
+*/
+
+
+int Prompt(string massege)
+{
+    System.Console.WriteLine(massege);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+bool PrimeNumber(int number)
+{
+    if (number > 1) ;
+
+    for (int i = 2; i < number; i++)
+    {
+        if (number % i == 0)
+            return false;
+        return true;
+    }
+
+    return false;
+}
+
+int N = Prompt("Введите число N: ");
+Console.WriteLine();
+
+if (PrimeNumber(N)) Console.WriteLine($"{N} -> Это простое число");
+else Console.WriteLine($"{N} -> Это не простое число");
+
